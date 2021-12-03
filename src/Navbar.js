2a1c2fae3591a17366d29b1529332da066bb6194
TrishaReal <trishasairenereal@gmail.com>
2021-12-03 18:10:27 +0100

    First Commit

---
+++ src/Navbar.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect, useRef } from "react";
+import logo from "./logo.svg";
+import { FaBars } from "react-icons/fa";
+import { links, SocialBar } from "./links";
+
+const Navbar = () => {
+  const [show, setShow] = useState(false);
+
+  //Utilizzando useRef e useEffect cercheremo di regolare laltezza del menu del'hamburger in maniera dinamica:
+  const linkContainerRef = useRef(null);
+  const linkListRef = useRef(null);
+
+  useEffect(() => {
+    const linkListHeight = linkListRef.current.getBoundingClientRect().height;
+    if (show) {
+      linkContainerRef.current.style.height = `${linkListHeight}px`;
+    } else {
+      linkContainerRef.current.style.height = `0px`;
+    }
+  }, [show]);
+
+  return (
+    <nav className="nav">
+      <header className="nav-header">
+        <div className="nav-brand">
+          <img src={logo} alt="logo" className="nav-logo" />
+          <h4>NAVBAR</h4>
+        </div>
+        <button className="btn nav-toggler" onClick={() => setShow(!show)}>
+          <FaBars className="nav-icon" />
+        </button>
+      </header>
+
+      <div
+        className={`links-container ${
+          show ? "links-container show" : "links-container"
+        }`}
+        ref={linkContainerRef}
+      >
+        <ul className="nav-links" ref={linkListRef}>
+          {links.map((el) => {
+            return (
+              <li key={el.id}>
+                <a href={el.url} alt={el.text}>
+                  {el.text}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="social-links">
+        <SocialBar />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
